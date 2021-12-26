b56729133a0c17761ff8179f4c0246293b8b3a8f
hyunkim <han.kim@kakao.com>
2021-12-26 14:09:28 +0900

    chapter4 (mustache)

--- src/main/java/com/jojoldu/book/springboot/web/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/web/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.springboot.web.service.posts;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 
@@ -27,8 +31,24 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
+
+    @Transactional
+    public void delete (Long id) {
+        //존재하는 Posts인지 확인을 위해 엔티티 조회 후 삭제
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts); //엔티티를 파라미터로 삭제할 수도 있고, deleteById로 살제할 수도 있다.
+    }
+
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지, 조회 기능만 남겨두어 조회속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList()); // List로 반환
+    }
+
 }
